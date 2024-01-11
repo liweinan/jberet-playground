2ff4310fa6458585041291b933548dc6a0b6dd8e
阿男 <l.weinan@gmail.com>
2024-01-12 01:40:43 +0800

    add jpa

--- jpa/src/test/java/MyTest.java
+++ jpa/src/test/java/MyTest.java
@@ -2,6 +2,9 @@ import jakarta.batch.operations.JobOperator;
 import jakarta.batch.runtime.BatchRuntime;
 import jakarta.batch.runtime.BatchStatus;
 import jakarta.batch.runtime.JobExecution;
+import org.jberet.operations.DelegatingJobOperator;
+import org.jberet.operations.JobOperatorImpl;
+import org.jberet.se.BatchSEEnvironment;
 import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
@@ -17,30 +20,33 @@ public class MyTest {
 
     @Test
     public void givenChunk_thenBatch_completesWithSuccess() throws Exception {
-        JobOperator jobOperator = BatchRuntime.getJobOperator();
+        JobOperatorImpl jobOperator = (JobOperatorImpl) ((DelegatingJobOperator) BatchRuntime.getJobOperator()).getDelegate();
+        var env = (BatchSEEnvironment) jobOperator.getBatchEnvironment();
+        env.getJobRepository();
+
         long executionId = jobOperator.start("simpleChunk", new Properties());
         JobExecution jobExecution = jobOperator.getJobExecution(executionId);
         jobExecution = keepTestAlive(jobExecution);
         assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
     }
 
-    @Test
-    public void givenBatchlet_thenBatch_completeWithSuccess() throws Exception {
-        JobOperator jobOperator = BatchRuntime.getJobOperator();
-        long executionId = jobOperator.start("simpleBatchletJob", new Properties());
-        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
-        jobExecution = keepTestAlive(jobExecution);
-        assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
-    }
-
-    @Test
-    public void givenPartition_thenBatch_completesWithSuccess() throws Exception {
-        JobOperator jobOperator = BatchRuntime.getJobOperator();
-        long executionId = jobOperator.start("myPartitionJob", new Properties());
-        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
-        jobExecution = keepTestAlive(jobExecution);
-        assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
-    }
+//    @Test
+//    public void givenBatchlet_thenBatch_completeWithSuccess() throws Exception {
+//        JobOperator jobOperator = BatchRuntime.getJobOperator();
+//        long executionId = jobOperator.start("simpleBatchletJob", new Properties());
+//        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
+//        jobExecution = keepTestAlive(jobExecution);
+//        assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
+//    }
+//
+//    @Test
+//    public void givenPartition_thenBatch_completesWithSuccess() throws Exception {
+//        JobOperator jobOperator = BatchRuntime.getJobOperator();
+//        long executionId = jobOperator.start("myPartitionJob", new Properties());
+//        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
+//        jobExecution = keepTestAlive(jobExecution);
+//        assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
+//    }
 
     private JobExecution keepTestAlive(JobExecution jobExecution) throws InterruptedException {
         int maxTries = 0;
