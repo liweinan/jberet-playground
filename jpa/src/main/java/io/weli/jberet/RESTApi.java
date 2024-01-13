3a7257bb00f0ad22ca81448bb1fbfff5b64c6db3
阿男 <l.weinan@gmail.com>
2024-01-14 02:19:52 +0800

    update

--- jpa/src/main/java/io/weli/jberet/RESTApi.java
+++ jpa/src/main/java/io/weli/jberet/RESTApi.java
@@ -1,8 +1,11 @@
 package io.weli.jberet;
 
+import jakarta.batch.operations.JobOperator;
 import jakarta.batch.runtime.BatchRuntime;
 import jakarta.batch.runtime.JobExecution;
 import jakarta.ws.rs.Path;
+
+import java.util.Properties;
 
 import static jakarta.batch.runtime.BatchStatus.COMPLETED;
 import static java.lang.Thread.sleep;
@@ -10,9 +13,13 @@ import static java.lang.Thread.sleep;
 @Path("/batch")
 public class RESTApi {
     @Path("/start")
-    public void start() {
+    public String start() throws InterruptedException {
         System.out.println("batch start");
-
+        JobOperator jobOperator = BatchRuntime.getJobOperator();
+        long executionId = jobOperator.start("simpleBatchletJob", new Properties());
+        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
+        jobExecution = keepTestAlive(jobExecution);
+        return jobExecution.getBatchStatus().toString();
 
     }
 
