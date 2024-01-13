a18fb46d5938a893adf19aa9536446c12587271d
阿男 <l.weinan@gmail.com>
2024-01-14 03:13:52 +0800

    complete testing

--- jpa/src/main/java/io/weli/jberet/RESTApi.java
+++ jpa/src/main/java/io/weli/jberet/RESTApi.java
@@ -3,6 +3,7 @@ package io.weli.jberet;
 import jakarta.batch.operations.JobOperator;
 import jakarta.batch.runtime.BatchRuntime;
 import jakarta.batch.runtime.JobExecution;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
 import java.util.Properties;
@@ -12,14 +13,26 @@ import static java.lang.Thread.sleep;
 
 @Path("/batch")
 public class RESTApi {
+    @GET
     @Path("/start")
-    public String start() throws InterruptedException {
+    public String start() {
         System.out.println("batch start");
         JobOperator jobOperator = BatchRuntime.getJobOperator();
         long executionId = jobOperator.start("simpleBatchletJob", new Properties());
-        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
-        jobExecution = keepTestAlive(jobExecution);
-        return jobExecution.getBatchStatus().toString();
+        try {
+            JobExecution jobExecution = jobOperator.getJobExecution(executionId);
+            System.out.println("jobExecution -> " + jobExecution);
+            keepTestAlive(jobExecution);
+            System.out.println("jobExecution -> " + jobExecution.getBatchStatus().toString());
+            return jobExecution.getBatchStatus().toString();
+        } catch (Exception e) {
+            System.out.println("e -> " + e);
+            return "ERR";
+        }
+
+
+
+
 
     }
 
--- jpa/src/test/java/io/weli/jberet/MyTestIT.java
+++ jpa/src/test/java/io/weli/jberet/MyTestIT.java
@@ -1,121 +1,17 @@
-package io.weli.jberet;//import jakarta.batch.operations.JobOperator;
-import jakarta.batch.runtime.BatchRuntime;
-import jakarta.batch.runtime.BatchStatus;
-import jakarta.batch.runtime.JobExecution;
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.EntityManagerFactory;
-//import jakarta.persistence.SharedCacheMode;
-//import jakarta.persistence.spi.PersistenceUnitTransactionType;
-//import org.h2.jdbcx.JdbcDataSource;
-//import org.hibernate.jpa.boot.spi.Bootstrap;
-//import org.jberet.jpa.repository.JpaRepository;
-//import org.jberet.jpa.repository.entity.JobExecutionJpa;
-//import org.jberet.jpa.repository.entity.JobInstanceJpa;
-//import org.jberet.jpa.repository.entity.PartitionExecutionJpa;
-//import org.jberet.jpa.repository.entity.StepExecutionJpa;
-//import org.jberet.jpa.util.BatchPersistenceUnitInfo;
-import org.jberet.operations.DelegatingJobOperator;
-import org.jberet.operations.JobOperatorImpl;
-import org.junit.jupiter.api.Disabled;
+package io.weli.jberet;
+
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-//import org.jberet.se.BatchSEEnvironment;
 
-//import java.util.Arrays;
-//import java.util.HashMap;
-//import java.util.Map;
-import java.util.Properties;
-//import java.util.stream.Collectors;
-
-import static jakarta.batch.runtime.BatchStatus.COMPLETED;
-import static java.lang.Thread.sleep;
-//import static org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO;
-//import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyTestIT {
 
-
-//    private static JobRepository repo;
-//
-//    private static EntityManagerFactory entityManagerFactory;
-//
-//    private static EntityManagerFactoryBuilder entityManagerFactoryBuilder;
-//    private static EntityManager entityManager;
-
-//
-//    public void setupDb() {
-//        JdbcDataSource ds = new JdbcDataSource();
-//        ds.setURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-//
-//        BatchPersistenceUnitInfo batchPersistenceUnitInfo = new BatchPersistenceUnitInfo();
-//        batchPersistenceUnitInfo.setPersistenceUnitName(MyTest.class.getSimpleName());
-//        batchPersistenceUnitInfo.setClassLoader(Thread.currentThread().getContextClassLoader());
-//        batchPersistenceUnitInfo.setProperties(new Properties());
-//        batchPersistenceUnitInfo.setNonJtaDataSource(ds);
-//        batchPersistenceUnitInfo.setTransactionType(PersistenceUnitTransactionType.RESOURCE_LOCAL);
-//        batchPersistenceUnitInfo.setSharedCacheMode(SharedCacheMode.ALL);
-//        batchPersistenceUnitInfo.setExcludeUnlistedClasses(false);
-//        batchPersistenceUnitInfo.setManagedClassNames(
-//                Arrays.asList(
-//                        JobInstanceJpa.class,
-//                        JobExecutionJpa.class,
-//                        StepExecutionJpa.class,
-//                        PartitionExecutionJpa.class
-//                ).stream().map(
-//                        c -> c.getCanonicalName()
-//                ).collect(Collectors.toList())
-//        );
-//
-//        Map integration = new HashMap<>();
-//        integration.put(HBM2DDL_AUTO, "create");
-//        integration.put(SHOW_SQL, "true");
-//
-//        entityManagerFactoryBuilder = Bootstrap.getEntityManagerFactoryBuilder(
-//                batchPersistenceUnitInfo,
-//                integration
-//        );
-//
-//        entityManagerFactory = entityManagerFactoryBuilder.build();
-//        entityManager = entityManagerFactory.createEntityManager();
-//
-//        repo = new JpaRepository(entityManager);
-//    }
-
-//    @Test
-//    @Disabled
-//    public void givenChunk_thenBatch_completesWithSuccess() throws Exception {
-//
-//        setupDb();
-//
-//        JobOperatorImpl jobOperator = (JobOperatorImpl) ((DelegatingJobOperator) BatchRuntime.getJobOperator()).getDelegate();
-//        var env = (BatchSEEnvironment) jobOperator.getBatchEnvironment();
-//        env.setJobRepository(repo);
-//
-//
-//        long executionId = jobOperator.start("simpleChunk", new Properties());
-//        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
-//        jobExecution = keepTestAlive(jobExecution);
-//        assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
-//    }
-//
-//    @Test
-//    public void givenBatchlet_thenBatch_completeWithSuccess() throws Exception {
-//        JobOperator jobOperator = BatchRuntime.getJobOperator();
-//        long executionId = jobOperator.start("simpleBatchletJob", new Properties());
-//        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
-//        jobExecution = keepTestAlive(jobExecution);
-//        assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
-//    }
-//
-//    @Test
-//    public void givenPartition_thenBatch_completesWithSuccess() throws Exception {
-//        JobOperator jobOperator = BatchRuntime.getJobOperator();
-//        long executionId = jobOperator.start("myPartitionJob", new Properties());
-//        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
-//        jobExecution = keepTestAlive(jobExecution);
-//        assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
-//    }
-
+    @Test
+    public void testApi() {
+        assertEquals("COMPLETED", RestAssured.given().baseUri("http://localhost/batch-jpa").basePath("/batch/start").port(8080)
+                .get().asString());
+    }
 
 
 }
