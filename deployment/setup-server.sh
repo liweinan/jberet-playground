8583ba97f1e8d0f42bb0feebbcb31f7dc5f2feb3
阿男 <l.weinan@gmail.com>
2024-09-02 00:24:06 +0800

    add script

--- deployment/setup-server.sh
+++ deployment/setup-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+# run it after running `mvn install` to provision the WildFly server.
 JBOSS_HOME=target/server
 
 $JBOSS_HOME/bin/add-user.sh -u admin -p admin
+$JBOSS_HOME/bin/jboss-cli.sh -c --commands='xa-data-source add --name=batch_db --enabled=true --use-java-context=true --use-ccm=true --jndi-name=java:jboss/jsr352/batch_db --xa-datasource-properties={"URL"=>"jdbc:h2:/tmp/batch_db"} --driver-name=h2 --user-name=sa	--password=sa --same-rm-override=false --no-recovery=true, /subsystem=batch-jberet/jdbc-job-repository=batch_db:add(data-source=batch_db), /subsystem=batch-jberet/:write-attribute(name=default-job-repository,value=batch_db)'
